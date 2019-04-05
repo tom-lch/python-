72fbb6a4c73ba337ae221f32678f05fa4413e4cb
李超 <samuel@lichaodeMacBook-Pro.local>
2019-04-05 09:28:51 +0800

    first commit

---
+++ stack/select_stack.py
@@ -0,0 +1,39 @@
+#使用sklearn训练模型找到合适的股票
+import numpy as np
+import pandas as pd
+import sklearn
+import tushare as ts
+
+class Select(object):
+    #初始化
+    def __init__(self):
+        pass
+
+    #导入数据
+    def input_data(self):
+        pass
+
+    #使用线性模型训练
+    def liner_model(self):
+        pass
+
+    #使用回归模型训练
+    def regression_model(self):
+        pass
+
+    #使用神经网络模型训练
+    def nn_model(self):
+        pass
+
+    #使用深度学习模型
+    def dnn_model(self):
+        pass
+    #使用强化学习模型
+    def sh_model(self):
+        pass
+
+    #比较各个模型得出的结论选择合适的股票
+    def select_one(self):
+        pass
+
+
